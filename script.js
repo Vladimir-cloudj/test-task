f8618989f5df4799f32aecccbc5f07738f1aa16a
Vladimir <wizart88@gmail.com>
2021-12-26 20:50:51 +0400

    init: start
     add style, index and img

---
+++ script.js
@@ -0,0 +1,80 @@
+// function fakeBin(x){
+// 	let arr = x.split('');
+// 	for (let i = 0; i < x.length; i++) {
+// 		if (x[i] >= 5) {
+// 			arr[i] = 1
+// 		} else {
+// 			arr[i] = 0
+// 		}
+// 	}
+// 	// x.split('').forEach(fu => {
+// 		// console.log(arr.join(''))
+// 	// })
+// 	return console.log(arr.join('')); 
+// }
+
+// fakeBin('1234456')
+
+
+// let x = 'btthth'
+// let arr = x.split('');
+// console.log(arr);
+
+
+
+// function digits(n) {
+// 	let result = 0;
+// 	if (n = 0) {
+// 		return console.log(result + 1);;
+// 	}
+// 	while (n > 0) {
+// 		n = Math.floor(n / 10)
+// 		result = result + 1
+// 	}
+// 	return console.log(result);
+
+// }
+
+// digits(3456)
+
+
+// function opposite(number) {
+// 	if (number === 0) {
+// 		return console.log(0);
+// 	}
+//   number = number * -1;
+// 	return console.log(number); 
+// }
+
+// opposite(0)
+
+
+// function removeChar(str){
+//   // let arr = str.split('');
+// 	return console.log(str.slice(1, str.length - 1)); 
+// // return console.log(arr.slice(1, arr.length - 1).join(''));
+// };
+
+// removeChar('abc');
+
+// return str.slice(1, str.length - 1)
+
+
+// function solution(str, ending){
+//   return console.log(str.substring((ending.length)) == ending);
+// }
+
+// solution('abcde', 'cde')
+
+
+// function simpleMultiplication(number) {
+// 	if (number % 2 == 0) {
+// 		return console.log(number * 8); 
+// 	} else return console.log(number * 9); 
+// }
+
+// simpleMultiplication(2);//16
+// simpleMultiplication(1);//9
+// simpleMultiplication(8);//64
+// simpleMultiplication(4);//32
+// simpleMultiplication(5);//45
